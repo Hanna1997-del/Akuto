ef0924e86636af52102518e29f0d1b990bbaf18d
Hanna1997-del <hninthethmue8@gmail.com>
2024-06-14 13:59:43 +0630

    fix navbar

--- app/components/Main/Navbar.tsx
+++ app/components/Main/Navbar.tsx
@@ -4,13 +4,13 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[30px]  z-30 flex justify-between items-center mt-5 '>
         <div className='flex items-center px-5'>
-           <div className='w-[50px] h-[50px] bg-[#ff3500] rounded-full '>
-            <div className='w-[20px] h-[20px] bg-[#ffffff] rounded-full absolute -top-2  left-8'></div>
+           <div className='w-[45px] h-[45px] bg-[#ff3500] rounded-full '>
+            <div className='w-[15px] h-[15px] bg-[#ffffff] rounded-full absolute -top-1  left-8'></div>
 
            </div>
-           <h1 className='text-3xl font-semibold'>Akuto Studio</h1>
+           <h1 className='text-3xl font-bold  uppercase px-3'>Akuto Studio</h1>
         </div>
-        <button className='bg-[#ff3500] w-[180px] rounded-full font-semibold h-[50px] text-2xl text-[#ffffff] mx-10 '>PRE-ORDER</button>
+        <button className='bg-[#ff3500] w-[150px] rounded-full font-semibold h-[50px] text-lg text-[#ffffff] mx-10 '>PRE-ORDER</button>
     </div>
   )
 }
